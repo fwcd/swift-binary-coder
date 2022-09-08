10c359a940758069cc1466ec8cff652cf6991fb3
fwcd <fwcdmail@gmail.com>
2022-09-08 21:43:46 +0300

    Add Endianness.platformDefault

--- Sources/BinaryCoder/Endianness.swift
+++ Sources/BinaryCoder/Endianness.swift
@@ -1,11 +1,13 @@
 /// The byte order of a fixed-width integer.
 public enum Endianness {
+    case platformDefault
     case bigEndian
     case littleEndian
 
     /// Converts to the endianness.
     func apply<Integer>(_ value: Integer) -> Integer where Integer: FixedWidthInteger {
         switch self {
+        case .platformDefault: return value
         case .bigEndian: return value.bigEndian
         case .littleEndian: return value.littleEndian
         }
@@ -14,6 +16,7 @@ public enum Endianness {
     /// Converts from the endianness to the platform's default endianness.
     func assume<Integer>(_ value: Integer) -> Integer where Integer: FixedWidthInteger {
         switch self {
+        case .platformDefault: return value
         case .bigEndian: return .init(bigEndian: value)
         case .littleEndian: return .init(littleEndian: value)
         }
