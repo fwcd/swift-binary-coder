aaa3fc989924a4f6b1020ed9cf42a0bb8cfe42cb
fwcd <fwcdmail@gmail.com>
2022-09-08 12:16:07 +0300

    Track count in UnkeyedBinaryEncodingContainer

--- Sources/BinaryCoder/Encoder/UnkeyedBinaryEncodingContainer.swift
+++ Sources/BinaryCoder/Encoder/UnkeyedBinaryEncodingContainer.swift
@@ -20,35 +20,83 @@ struct UnkeyedBinaryEncodingContainer: UnkeyedEncodingContainer {
         BinaryEncoderImpl(state: state)
     }
 
-    mutating func encodeNil() throws { try state.encodeNil() }
+    mutating func encodeNil() throws {
+        try state.encodeNil()
+        count += 1
+    }
 
-    mutating func encode(_ value: Bool) throws { try state.encode(value) }
+    mutating func encode(_ value: Bool) throws {
+        try state.encode(value)
+        count += 1
+    }
 
-    mutating func encode(_ value: String) throws { try state.encode(value) }
+    mutating func encode(_ value: String) throws {
+        try state.encode(value)
+        count += 1
+    }
 
-    mutating func encode(_ value: Double) throws { try state.encode(value) }
+    mutating func encode(_ value: Double) throws {
+        try state.encode(value)
+        count += 1
+    }
 
-    mutating func encode(_ value: Float) throws { try state.encode(value) }
+    mutating func encode(_ value: Float) throws {
+        try state.encode(value)
+        count += 1
+    }
 
-    mutating func encode(_ value: Int) throws { try state.encode(value) }
+    mutating func encode(_ value: Int) throws {
+        try state.encode(value)
+        count += 1
+    }
 
-    mutating func encode(_ value: Int8) throws { try state.encode(value) }
+    mutating func encode(_ value: Int8) throws {
+        try state.encode(value)
+        count += 1
+    }
 
-    mutating func encode(_ value: Int16) throws { try state.encode(value) }
+    mutating func encode(_ value: Int16) throws {
+        try state.encode(value)
+        count += 1
+    }
 
-    mutating func encode(_ value: Int32) throws { try state.encode(value) }
+    mutating func encode(_ value: Int32) throws {
+        try state.encode(value)
+        count += 1
+    }
 
-    mutating func encode(_ value: Int64) throws { try state.encode(value) }
+    mutating func encode(_ value: Int64) throws {
+        try state.encode(value)
+        count += 1
+    }
 
-    mutating func encode(_ value: UInt) throws { try state.encode(value) }
+    mutating func encode(_ value: UInt) throws {
+        try state.encode(value)
+        count += 1
+    }
 
-    mutating func encode(_ value: UInt8) throws { try state.encode(value) }
+    mutating func encode(_ value: UInt8) throws {
+        try state.encode(value)
+        count += 1
+    }
 
-    mutating func encode(_ value: UInt16) throws { try state.encode(value) }
+    mutating func encode(_ value: UInt16) throws {
+        try state.encode(value)
+        count += 1
+    }
 
-    mutating func encode(_ value: UInt32) throws { try state.encode(value) }
+    mutating func encode(_ value: UInt32) throws {
+        try state.encode(value)
+        count += 1
+    }
 
-    mutating func encode(_ value: UInt64) throws { try state.encode(value) }
+    mutating func encode(_ value: UInt64) throws {
+        try state.encode(value)
+        count += 1
+    }
 
-    mutating func encode<T>(_ value: T) throws where T : Encodable { try state.encode(value) }
+    mutating func encode<T>(_ value: T) throws where T: Encodable {
+        try state.encode(value)
+        count += 1
+    }
 }
