bbb9dd87d9d1fc88eb96d174e0c1342c603861b7
fwcd <fwcdmail@gmail.com>
2022-09-10 21:33:46 +0300

    Make FixedSizeArray a property wrapper

--- Sources/BinaryCoder/Utils/FixedSizeArray.swift
+++ Sources/BinaryCoder/Utils/FixedSizeArray.swift
@@ -1,6 +1,17 @@
 /// An array with a statically known length whose length is enforced at runtime.
+@propertyWrapper
 public struct FixedSizeArray<Count, Value>: Sequence where Count: TypeLevelInt {
-    public let values: [Value]
+    public var values: [Value]
+
+    public var wrappedValue: [Value] {
+        get { values }
+        set {
+            guard newValue.count == Count.value else {
+                fatalError("Cannot assign \(newValue) to a fixed-size array with count \(Self.count)")
+            }
+            values = newValue
+        }
+    }
 
     public static var count: Int { Count.value }
 
@@ -11,6 +22,13 @@ public struct FixedSizeArray<Count, Value>: Sequence where Count: TypeLevelInt {
     public init?(_ values: [Value]) {
         guard values.count == Count.value else { return nil }
         self.values = values
+    }
+
+    public init(wrappedValue values: [Value]) {
+        guard let array = Self(values) else {
+            fatalError("Number of values in \(values) did not match the expected count \(Self.count)")
+        }
+        self = array
     }
 
     public func makeIterator() -> Array<Value>.Iterator {
--- Tests/BinaryCoderTests/FixedSizeArrayTests.swift
+++ Tests/BinaryCoderTests/FixedSizeArrayTests.swift
@@ -8,5 +8,9 @@ final class FixedSizeArrayTests: XCTestCase {
         XCTAssertEqual(FixedSizeArray<TypeLevelZero.I.O.I, Int>(repeating: 2).values, [2, 2, 2, 2, 2])
         XCTAssertEqual(Array(FixedSizeArray<TypeLevelZero.I.O.I, Int>([1, 2, 3, 4, 5])!), [1, 2, 3, 4, 5])
         XCTAssertNil(FixedSizeArray<TypeLevelZero.I.O.I, Int>([1, 2, 3]))
+
+        @FixedSizeArray<TypeLevelZero.I.O.O, Int> var wrapped = [1, 2, 3, 4]
+        wrapped = [0, 0, 1, 3]
+        XCTAssertEqual(wrapped, [0, 0, 1, 3])
     }
 }
